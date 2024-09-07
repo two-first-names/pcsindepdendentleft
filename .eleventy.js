45a7822df15349766feb261f9760fe2bc14c4c09
Joe Roberts <joe@engiqueer.net>
2024-09-07 21:25:38 +0100

    Add CSS and basic layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy({ "wpsrc/wp-content": "wp-content"});
 }
